4e6a15b8f2e3808d60a75a4267e3aabe99fbd6fc
2300033794 <2300033794@kluniversity.in>
2025-11-20 14:42:11 +0530

    v4.0

--- server/src/server.js
+++ server/src/server.js
@@ -57,3 +57,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`✅ Server running on http://localhost:${PORT}`)
 );
+
+
+//admin==> admin@gmail.com
+//password==> 123456
